b15f9ca94f8c2dccee9809d5bba6c7d7ac55bb89
SachinBalagam <balagamsachin337@gmail.com>
2023-07-24 17:11:35 +0000

    Final Commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const Button = props => {
-  //  Write your code here.
-}
+const Button = (props) => {
+  const { name, buttonCss } = props;
+  return <button className={`button ${buttonCss}`}>{name}</button>;
+};
 
 const element = (
-  //  Write your code here.
-)
+  <div className="bg-container">
+    <h1 className="title">Social Buttons</h1>
+    <div className="card">
+      <Button name="Like" buttonCss="like" />
+      <Button name="Comment" buttonCss="comment" />
+      <Button name="Share" buttonCss="share" />
+    </div>
+  </div>
+);
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById("root"));
